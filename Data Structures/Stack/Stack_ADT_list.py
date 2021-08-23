d8f07dd21394f61813cc942fded7cf2e79334c71
Raj Aryan <81006469+imRajAryan09@users.noreply.github.com>
2021-08-23 22:57:05 +0530

    Update Stack_ADT_list.py

--- Data Structures/Stack/Stack_ADT_list.py
+++ Data Structures/Stack/Stack_ADT_list.py
@@ -1,20 +1,44 @@
-# Completed implementation of a stack ADT using a list 
+# Python program for linked list implementation of stack
+
+# Class to represent a node
+class StackNode:
+
+	# Constructor to initialize a node
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+
+
 class Stack:
-    
-    def __init__(self):
-        self.items = []
+
+	# Constructor to initialize the root of linked list
+	def __init__(self):
+		self.root = None
         
-    def is_empty(self):
-        return self.items == []
+    # Stack is empty when stack size is 0
+	def isEmpty(self):
+		return True if self.root is None else False
     
-    def push(self, item):
-        self.items.append(item)
+    # Function to add an item to stack. It increases size by 1
+	def push(self, data):
+		newNode = StackNode(data)
+		newNode.next = self.root
+		self.root = newNode
+		print "% d pushed to stack" % (data)
         
-    def pop(self):
-        return self.items.pop()
+    # Function to remove an item from stack. It decreases size by 1
+	def pop(self):
+		if (self.isEmpty()):
+			return float("-inf")
+		temp = self.root
+		self.root = self.root.next
+		popped = temp.data
+		return popped
     
-    def peek(self):
-        return self.items[len(self.items)-1]
-    
-    def size(self):
-        return len(self.items)
+    # Function to return the top from stack without removing it
+	def peek(self):
+		if self.isEmpty():
+			return float("-inf")
+		return self.root.data
+
+
