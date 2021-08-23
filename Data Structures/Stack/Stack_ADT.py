47e1862e336da0dde39c161f92c25bdd3bf3e49b
Raj Aryan <81006469+imRajAryan09@users.noreply.github.com>
2021-08-23 22:26:36 +0530

    Update Stack_ADT.py

--- Data Structures/Stack/Stack_ADT.py
+++ Data Structures/Stack/Stack_ADT.py
@@ -3,17 +3,17 @@ class Stack:
     def __init__(self):
         self.items = []
         
-    def is_empty(self):
+    def is_empty(self):                     # tests to see whether the stack is empty
         return self.items == []
     
-    def push(self, item):
+    def push(self, item):                   # adds a new item to the top of the stack
         self.items.insert(0, item)
         
-    def pop(self):
+    def pop(self):                          # removes the top item from the stack
         return self.items.pop(0)
     
-    def peek(self):
+    def peek(self):                         # returns the top item from the stack
         return self.items[0]
     
-    def size(self):
+    def size(self):                         # returns the number of items on the stack
         return len(self.items)
